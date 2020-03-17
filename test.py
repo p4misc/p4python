1ae40f86fde685a2ed99f2de7c59006765fa14e9
p4misc <54461526+p4misc@users.noreply.github.com>
2020-03-17 09:15:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
 from P4 import P4,P4Exception
 p4 = P4()
+p4.port="host.docker.internal:1666"
+#p4.user="bruno"
+#p4.client="bruno_ws"
+
+try:
+    p4.connect()
+    info = p4.run("info")
+    for key in info[0]:
+        print(key, "=", info[0][key])
+    p4.disconnect()
+except P4Exception:
+    for e in p4.errors:
+        print(e)
